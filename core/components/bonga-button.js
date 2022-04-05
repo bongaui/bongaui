a06e4a215978e52d3b700634d6cdaa7405d51934
Sebastian Gomez <seagomezar@gmail.com>
2022-04-05 13:00:35 -0500

    Creating the storybook action

--- core/components/bonga-button.js
+++ core/components/bonga-button.js
@@ -1,4 +1,4 @@
-import { HTMLElement, h, Host, proxyCustomElement } from '@stencil/core/internal/client';
+import { proxyCustomElement, HTMLElement, h, Host } from '@stencil/core/internal/client';
 
 /**
  * Create the mode and color classes for the component based on the classes passed in
@@ -10,7 +10,7 @@ const createColorClasses = (color, cssClassMap) => {
 
 const bongaButtonCss = ":host{border-radius:48px;display:inline-block;width:auto;text-align:center;text-decoration:none;text-overflow:ellipsis;white-space:nowrap;user-select:none;vertical-align:top;vertical-align:-webkit-baseline-middle;pointer-events:auto;font-kerning:none}:host(.bonga-color-primary){background:#c5b787}:host(.bonga-color-primary) slot{color:white}:host(.bonga-color-secondary){background:#55943a}:host(.bonga-color-secondary) slot{color:white}:host(.bonga-color-tertiary){background:#4a5da7}:host(.bonga-color-tertiary) slot{color:white}:host(.button-block){display:block}:host(.button-block) .button-native{margin:0;display:block;width:100%;clear:both;contain:content}:host(.button-block) .button-native::after{clear:both}:host(.button-full){display:block}:host(.button-full) .button-native{margin:0;display:block;width:100%;contain:content}:host(.button-full) .button-native{border-radius:0;border-right-width:0;border-left-width:0}.button-native{background:transparent;font-size:16px;padding:8px 20px;border-radius:var(--border-radius);border:none;outline:none;cursor:pointer;contain:layout style;line-height:1;display:block;position:relative;height:100%}";
 
-let BongaButton$1 = class extends HTMLElement {
+const BongaButton$1 = /*@__PURE__*/ proxyCustomElement(class extends HTMLElement {
   constructor() {
     super();
     this.__registerHost();
@@ -25,8 +25,7 @@ let BongaButton$1 = class extends HTMLElement {
   }
   get el() { return this; }
   static get style() { return bongaButtonCss; }
-};
-BongaButton$1 = /*@__PURE__*/ proxyCustomElement(BongaButton$1, [1, "bonga-button", {
+}, [1, "bonga-button", {
     "color": [513],
     "expand": [513]
   }]);
